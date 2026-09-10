41628fe1da111ca548f12f16e4054c9ae8cd14cf
Andrew Harrison <andrew@nosirrah.co.uk>
2026-09-10 17:18:20 +0100

    UPDATED Expression to contain an expected override for toString as this is expected for any expression being used in the Cora String builder

--- src/LCTRSTree/Expressions/Expression.java
+++ src/LCTRSTree/Expressions/Expression.java
@@ -1,5 +1,11 @@
 package LCTRSTree.Expressions;
 
-public interface Expression {
+/**
+ * Interface class used to identify an expression, any new expression files should implement this empty interface to be
+ * allowed to be used as an expression. Any expression must implement their own versions of the to String method
+ */
 
+public interface Expression {
+    @Override
+    public String toString();
 }
