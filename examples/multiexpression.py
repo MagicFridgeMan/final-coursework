89284a70508cb5884b4d5bd70269410f05987e2e
Andrew Harrison <andrew@nosirrah.co.uk>
2026-09-13 19:27:16 +0100

    Created tests for multiple assignments within the bodies of the python program

---
+++ examples/multiexpression.py
@@ -0,0 +1,4 @@
+def multi_expression(x: int) -> int:
+    y = x + 1
+    z = y + 2
+    return y
---
+++ examples/multiwhile.py
@@ -0,0 +1,7 @@
+def multiwhile(x: int) -> int:
+    y = 0
+    z = 1
+    while (x > y):
+        y = y + 1
+        z = z * 2
+    return z
