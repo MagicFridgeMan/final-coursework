1d56c70bfdd87fcaa0195df80d89a2a0424b7d3a
Andrew Harrison <andrew@nosirrah.co.uk>
2026-09-13 19:46:34 +0100

    CREATED the main program to handle taking an input and output on the command line.
    Program requires the first argument to be a Python file and the 2nd to be the Name of the file you want to output to.

---
+++ src/PythonToCora.java
@@ -0,0 +1,43 @@
+import LCTRSTree.CoraStringBuilder;
+import LCTRSTree.Visitors.FunctionVisitor;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import parser.LctrsLexer;
+import parser.LctrsParser;
+
+void main(String[] args) {
+    {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Wrong number of arguments");
+        }
+
+        if (!args[0].endsWith(".py")) {
+            throw new IllegalArgumentException("Input file must be a python file");
+        }
+
+        String fileName = args[0];
+        String outname = args[1];
+
+        try {
+            File file = new File(fileName);
+            FileInputStream fis = new FileInputStream(file);
+            ANTLRInputStream input = new ANTLRInputStream(fis);
+            LctrsLexer lexer = new LctrsLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            LctrsParser parser = new LctrsParser(tokens);
+            FunctionVisitor visitor = new FunctionVisitor();
+            LctrsParser.FuncdefContext tree = parser.funcdef();
+            visitor.visit(tree);
+            IO.println(tree.toStringTree(parser));
+            CoraStringBuilder builder = new CoraStringBuilder(visitor.getProgram());
+            IO.println(builder.toCoraString());
+            CoraFileWriter.Write(builder.toCoraString(), outname);
+            IO.println("Cora program written to " + outname + ".txt");
+            fis.close();
+        } catch (IOException e) {
+            IO.println("Could not open file " + fileName);
+        }
+
+
+    }
+}
